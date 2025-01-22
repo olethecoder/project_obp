3b21e5a9159ed4f4cad8b7d8b71e901462c378d7
Rogier <rogierdalhuisen@gmail.com>
2025-01-22 20:42:21 +0100

    DIT IS DE LAATSTE VERSIE VALIDATOR

--- unit_test.py
+++ unit_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-class UnitTest():
+class Validator():
 
     def __init__(self, shifts_df, tasks_df):
         self.shifts = shifts_df[shifts_df["usage"] != 0].copy()
@@ -25,8 +25,8 @@ class UnitTest():
     @staticmethod
     def get_end_day(start_time, end_time, start_day_index):
 
-        start_quarter = UnitTest.to_quarter_of_day(start_time)
-        end_quarter = UnitTest.to_quarter_of_day(end_time)
+        start_quarter = Validator.to_quarter_of_day(start_time)
+        end_quarter = Validator.to_quarter_of_day(end_time)
 
         if end_quarter < start_quarter:
             return (start_day_index + 1) % 7
@@ -36,29 +36,29 @@ class UnitTest():
     @staticmethod
     def get_shift_index(start_time, end_time, start_day_index, start_time_break, break_duration):
 
-        start_quarter = UnitTest.to_quarter_of_day(start_time)
-        end_quarter   = UnitTest.to_quarter_of_day(end_time)
+        start_quarter = Validator.to_quarter_of_day(start_time)
+        end_quarter   = Validator.to_quarter_of_day(end_time)
 
-        end_day_index = UnitTest.get_end_day(start_time, end_time, start_day_index)
+        end_day_index = Validator.get_end_day(start_time, end_time, start_day_index)
 
         start_index = (start_day_index * 96 + start_quarter + 1) % 672     #+1 because at beginning of shift nurse is briefed
         end_index   = end_day_index * 96 + end_quarter
 
         #break 
-        start_break_quarter = UnitTest.to_quarter_of_day(start_time_break)
-        start_break_day_index = UnitTest.get_end_day(start_time, start_time_break, start_day_index)
+        start_break_quarter = Validator.to_quarter_of_day(start_time_break)
+        start_break_day_index = Validator.get_end_day(start_time, start_time_break, start_day_index)
 
         start_break_index = start_break_day_index * 96 + start_break_quarter
-        end_break_index = (start_break_index + UnitTest.time_length(break_duration)) % 672
+        end_break_index = (start_break_index + Validator.time_length(break_duration)) % 672
 
         return start_index, end_index, start_break_index, end_break_index
     
     @staticmethod
     def get_task_index(start_time, start_day_index, duration):
         
-        start_quarter = UnitTest.to_quarter_of_day(start_time)
+        start_quarter = Validator.to_quarter_of_day(start_time)
         start_index = start_day_index * 96 + start_quarter
-        end_index = (start_index + UnitTest.time_length(duration)) % 672
+        end_index = (start_index + Validator.time_length(duration)) % 672
         return start_index, end_index
 
     
@@ -67,7 +67,7 @@ class UnitTest():
             for day in self.days:
                 if shift[day] == 1:
                     usage = int(shift["usage"])
-                    start_index, end_index, start_break_index, end_break_index = UnitTest.get_shift_index(shift["start"], shift["end"], self.days.index(day), shift["break"], shift["break_duration"])
+                    start_index, end_index, start_break_index, end_break_index = Validator.get_shift_index(shift["start"], shift["end"], self.days.index(day), shift["break"], shift["break_duration"])
                     if start_index < end_index:
                         self.shifts_coverage[start_index:start_break_index] += usage
                         self.shifts_coverage[end_break_index:end_index] += usage
@@ -82,7 +82,7 @@ class UnitTest():
                         self.shifts_coverage[start_index:] += usage
                         self.shifts_coverage[:start_break_index] += usage
                         self.shifts_coverage[end_break_index:end_index] += usage
-        print(self.shifts_coverage)
+        
         return self.shifts_coverage
     
     def get_unique_start_times(self, df):
@@ -107,19 +107,18 @@ class UnitTest():
 
             required_nurses = task["required_nurses"]
             
-            start_index, end_index = UnitTest.get_task_index(task["solution_start"], task["day_index"], task["duration"])
+            start_index, end_index = Validator.get_task_index(task["solution_start"], task["day_index"], task["duration"])
             if start_index < end_index:
                 self.tasks_coverage[start_index:end_index] += required_nurses
             else:
                 self.tasks_coverage[start_index:] += required_nurses
                 self.tasks_coverage[:end_index] += required_nurses
         
-        shift_briefing = UnitTest.get_unique_start_times(self, self.shifts)       #1 nurse to brief the starting shifts
+        shift_briefing = Validator.get_unique_start_times(self, self.shifts)       #1 nurse to brief the starting shifts
         for _, row in shift_briefing.iterrows():
-            start_index, _ = UnitTest.get_task_index(row['start_time'], self.days.index(row['day']), 0)
+            start_index, _ = Validator.get_task_index(row['start_time'], self.days.index(row['day']), 0)
             self.tasks_coverage[start_index] += 1
         
-        print(self.tasks_coverage)
         return self.tasks_coverage
 
     def check_coverage(self):
@@ -130,4 +129,67 @@ class UnitTest():
             if coverage[i] < 0:
                 print(f"{-coverage[i]} more nurses needed at index {i}")
                 all_valid = False
+
+        if all_valid:
+            print("Shift coverage is valid")
         return all_valid
+    
+    def task_in_window(self):
+        all_valid = True
+
+        for _, task in self.tasks.iterrows():
+            start_window = task["start_window"]
+            end_window = task["end_window"]
+            start_solution = task["solution_start"]
+
+            if start_window <= end_window:
+                if start_window <= start_solution <= end_window:
+                    continue
+                else:
+                    print(f"Task {task['original_task_idx']} not in window")
+                    all_valid = False
+            else:
+                if start_window <= start_solution or start_solution <= end_window:
+                    continue
+                else:
+                    print(f"Task {task['original_task_idx']} not in window")
+                    all_valid = False
+        if all_valid:
+            print("All tasks are in window")
+        return all_valid
+    
+    def check_max_nurses(self):
+
+        all_valid = True
+        for _, shift in self.shifts.iterrows():
+            usage = int(shift["usage"])
+            max_nurses = shift["max_nurses"]
+            if usage > max_nurses:
+                print(f"Shift {shift['original_shift_idx']} has more nurses than allowed")
+                all_valid = False
+        if all_valid:
+            print("All shifts don't exceed maximum number of nurses")
+        return all_valid
+    
+    def validate_schedule(shifts, tasks):
+        validator = Validator(shifts, tasks)
+        validator.shift_coverage()
+        validator.task_coverage()
+        
+        print("Checking nurses coverage:")
+        coverage_valid = validator.check_coverage()
+        print()
+
+        print("Checking tasks in window:")
+        window_valid = validator.task_in_window()
+        print()
+
+        print("Checking maximum number of nurses")
+        max_nurses_valid = validator.check_max_nurses()
+        print()
+
+        all_valid = coverage_valid and window_valid and max_nurses_valid
+        if all_valid:
+            print("Schedule is valid")
+        else:
+            print("Schedule is invalid")
