dc38326e5bca88f98406269577b8162bd0414b81
olethecoder <ole@vriethoff.nl>
2025-01-30 10:20:00 +0100

    Final Gurobi Solver (fixed all corner cases)

--- gurobi_solver.py
+++ gurobi_solver.py
@@ -57,7 +57,6 @@ class GurobiNurseSolver:
         self.tasks_info = tasks_info
         self.task_map = task_map
         self.min_nurses_anytime = min_nurses_anytime
-      
 
         # Create the Gurobi model
         self.model = gp.Model("NurseScheduling_Gurobi")
@@ -79,19 +78,22 @@ class GurobiNurseSolver:
     def _build_model(self):
         """Build all constants, variables, objective, and constraints in the Gurobi model."""
         
-        
+        ### CONSTANTS ###
+
+        # Binary constants indicating whether shift j covers time block t
         self.e = {}
         for j in self.S:
             coverage_array = self.shift_info[j - 1]["coverage"]
             for t in self.T:
                 self.e[j, t] = coverage_array[t - 1] if (t - 1 < len(coverage_array)) else 0
 
+        # Binary constants indicating whether shift j has a starting point at time block t
         self.h = {}
         for t in self.T:
             for j in self.S:
                 self.h[j,t] = 1 if j-1 in self.starting_blocks[t-1] else 0
 
-        #Build candidate blocks for tasks
+        # Build candidate blocks for tasks
         self.candidate_blocks = []
         max_T = max(self.T)
         for i in self.N:
@@ -104,7 +106,9 @@ class GurobiNurseSolver:
                 for j in range(eb,lb+1):
                     self.candidate_blocks[i-1].append([])
                     for k in range(0,task['duration_blocks']):
-                        self.candidate_blocks[i-1][c_idx].append(j+k)
+                        if k + j <= max_T - 1:
+                            self.candidate_blocks[i-1][c_idx].append(j+k)
+                        else: self.candidate_blocks[i-1][c_idx].append(j+k - max_T) 
                     c_idx += 1
             else:
                 for j in range(eb,max_T-1+1):
@@ -119,8 +123,9 @@ class GurobiNurseSolver:
                     for k in range(0,task['duration_blocks']):
                        self. candidate_blocks[i-1][c_idx].append(j+k)
                     c_idx += 1
+        print(self.candidate_blocks[4])
 
-        # Build g[i, b, t]
+        # Binary constants indicating whether candidate task block b for task i covers time block t
         self.g = {}
         for i in self.N:
             c_size = len(self.candidate_blocks[i - 1])
@@ -129,7 +134,15 @@ class GurobiNurseSolver:
                 for t in self.T:
                     self.g[i, b + 1, t] = 1 if t-1 in covered_list else 0
 
-        # Decision variables
+        # Adaptive "Big_M" for calculating whether nurse needs to *provide* handover
+        self.max_starting_nurses = {}
+        for t in self.T:
+           self.max_starting_nurses[t] = sum(self.h[j,t] * self.shift_info[j-1]["max_nurses"] for j in self.S)
+        self.Big_M = max(self.max_starting_nurses.values())
+
+        ### DECISION VARIABLES ###
+
+        # Decision variables indicating whether candidate time block b for task i is activated
         self.f = {}
         for i in self.N:
             c_size = len(self.candidate_blocks[i - 1])
@@ -138,6 +151,7 @@ class GurobiNurseSolver:
                     vtype=GRB.BINARY, name=f"f_{i}_{b}"
                 )
 
+        # Decision variables indicating whehther task i is active at time block t
         self.u = {}
         for i in self.N:
             for t in self.T:
@@ -145,6 +159,7 @@ class GurobiNurseSolver:
                     vtype=GRB.BINARY, name=f"u_{i}_{t}"
                 )
 
+        # Decision variables indicating how many times shift schedule j is scheduled
         self.k = {}
         for j in self.S:
             self.k[j] = self.model.addVar(
@@ -154,31 +169,36 @@ class GurobiNurseSolver:
                 name=f"k_{j}"
             )
 
+        # Decision variables indicating required coverage (required number of nurses) at time t
         self.x = {}
         for t in self.T:
             self.x[t] = self.model.addVar(
                 vtype=GRB.INTEGER, name=f"x_{t}"
             )
 
+        # Decision variables indicating number of nurses present at time t
         self.n = {}
         for t in self.T:
             self.n[t] = self.model.addVar(
                 vtype=GRB.INTEGER, name=f"n_{t}"
             )
 
+        # Decision variables indicating number of nurses that need to *receive* a handover (briefing) at time t
         self.r = {}
         for t in self.T:
             self.r[t] = self.model.addVar(
                 vtype=GRB.INTEGER, name=f"r_{t}"
             )
 
+        # Decision variables indicating whether a nurse needs to *provide* a handover (briefing) at time t
         self.p = {}
         for t in self.T:
             self.p[t] = self.model.addVar(
                 vtype=GRB.BINARY, name=f"p_{t}"
             )
 
-        # Objective
+        ### OBJECTIVE ###
+
         obj_expr = gp.quicksum(
             self.k[j]
             * self.shift_info[j - 1]["weight_scaled"] / 100.0
@@ -187,64 +207,42 @@ class GurobiNurseSolver:
         )
         self.model.setObjective(obj_expr, GRB.MINIMIZE)
 
-        # Constraints
-        # 1) Each task picks exactly one candidate start
+        ###  CONSTRAINTS ###   
+        
+        # 1) Each task picks exactly one candidate time block
         for i in self.N:
             c_size = len(self.candidate_blocks[i - 1])
-            self.model.addConstr(
-                gp.quicksum(self.f[i, b] for b in range(1, c_size + 1)) == 1,
-                name=f"OneCandidate_{i}"
-            )
+            self.model.addConstr(gp.quicksum(self.f[i, b] for b in range(1, c_size + 1)) == 1)
 
-        # 2) If task i is active at time t => sum of candidate covering t
+        # 2) Task i is active at time block t if the chosen time block for task i covers time block t
         for i in self.N:
             c_size = len(self.candidate_blocks[i - 1])
             for t in self.T:
-                self.model.addConstr(
-                    self.u[i, t] >= gp.quicksum(
-                        self.f[i, b] * self.g[i, b, t]
-                        for b in range(1, c_size + 1)
-                    ),
-                    name=f"TaskActive_{i}_{t}"
-                )
+                self.model.addConstr(self.u[i, t] >= gp.quicksum(self.f[i, b] * self.g[i, b, t] for b in range(1, c_size + 1)))
 
-        # 3) total tasks plus handover in x[t]
+        # 3) Number of nurses *receiving* handover (briefing) at time block t equals number of scheduled nurses that have a shift start in their schedules at time block t
         for t in self.T:
-            sum_task_demand = gp.quicksum(
-                self.u[i, t] * self.tasks_info[i - 1]["required_nurses"]
-                for i in self.N
-            )
-        
             self.model.addConstr(self.r[t] == gp.quicksum(self.k[j] * self.h[j, t] for j in self.S)) 
-
-            self.model.addConstr(self.p[t] >= self.r[t]/50) 
-
-            self.model.addConstr(
-                self.x[t] >= sum_task_demand + self.r[t] + self.p[t],
-                name=f"CoverageReq_{t}"
-            )
-            # also global min coverage
-            self.model.addConstr(
-                self.x[t] >= self.min_nurses_anytime,
-                name=f"GlobalMin_{t}"
-            )
-
-        # 4) coverage from shifts
+        
+        # 4) 1 nurse needs to *provide* handover (briefing) if there are 1 or more nurses that need to *receive* handover
         for t in self.T:
-            self.model.addConstr(
-                self.n[t] == gp.quicksum( #### CHECK ####
-                    self.e[j, t] * self.k[j]
-                    for j in self.S
-                ),
-                name=f"ShiftCoverage_{t}"
-            )
+            self.model.addConstr(self.p[t] >= self.r[t]/self.Big_M) 
 
-        # 5) ensure n[t] >= x[t]
+        # 5) Required nurse coverage at time block t is greater or equal than task demand at time block t + nurses that receive handover at time block t + nurse that provides handover at time block t
         for t in self.T:
-            self.model.addConstr(
-                self.n[t] >= self.x[t],
-                name=f"NurseCover_{t}"
-            )
+            self.model.addConstr(self.x[t] >= gp.quicksum(self.u[i, t] * self.tasks_info[i - 1]["required_nurses"] for i in self.N) + self.r[t] + self.p[t])
+
+        # 6) PSEUDO-TASK: Required nurse coverage is always greater or equal than the minimum nurses present at all time
+        for t in self.T:
+            self.model.addConstr(self.x[t] >= self.min_nurses_anytime)
+
+        # 7) Nurses present at time t is equal to sum of present nurses at time t for all active nurse schedules
+        for t in self.T:
+            self.model.addConstr(self.n[t] == gp.quicksum(self.e[j, t] * self.k[j] for j in self.S))
+
+        # 8) Nurses present at time block t needs to cover the required nurse coverage at time block t
+        for t in self.T:
+            self.model.addConstr(self.n[t] >= self.x[t])
 
     def solve(self):
         """Solve the Gurobi model and store the results internally.
@@ -260,7 +258,7 @@ class GurobiNurseSolver:
             print("No feasible or optimal Gurobi solution found.")
             return (None, None)
 
-        # SHIFT usage solution
+        # Shift schedules usage solution
         self.usage_values = [int(self.k[j].X) for j in self.S]
 
         # Alter DataFrame for the shifts solution
